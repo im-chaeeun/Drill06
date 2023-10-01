f7a42d9f11358c1ae1047a9f808fea71a98208c7
im-chaeeun <ce0118@tukorea.ac.kr>
2023-10-01 14:03:40 +0900

    마우스 클릭 스탬프 구현
    여러개 해야 함
    
    event.button을 event.type로 잘못 써서 그려지지 않았었음

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -5,39 +5,45 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
-
+mouse = load_image('hand_arrow.png')
 
 def handle_events():
     global running
-    global x, y
+    global mouse_x, mouse_y, mouse_num, click_mouse_x, click_mouse_y
+
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
+            mouse_x, mouse_y = event.x, TUK_HEIGHT - 1 - event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            if event.button == SDL_BUTTON_LEFT:
+                mouse_num += 1
+                click_mouse_x, click_mouse_y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
 
-
-
 running = True
-x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+mouse_x, mouse_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+click_mouse_x, click_mouse_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
+mouse_num = 0
 hide_cursor()
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    mouse.clip_draw(0, 0, 50, 52, mouse_x, mouse_y)
+    if mouse_num > 0:
+        mouse.clip_draw(0, 0, 50, 52, click_mouse_x, click_mouse_y)
     update_canvas()
     frame = (frame + 1) % 8
 
     handle_events()
 
 close_canvas()
-
 
 
 
